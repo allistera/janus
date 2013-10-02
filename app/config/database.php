f067cda81dd981954dec28497d680b97176a1af1
Allister Antosik <allistera@gmail.com>
2013-10-02 19:15:36 +0100

    Changed database password

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'janus',
 			'username'  => 'root',
-			'password'  => 'root',
+			'password'  => 'sCRY07NO819r77v',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
